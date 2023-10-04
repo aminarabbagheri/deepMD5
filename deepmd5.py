7fe338b7f9a0787550457c726a1a5a8295688e80
Amin Arab Bagheri <amin.arabbagheri@gmail.com>
2023-10-04 20:09:58 +0330

    chatGPT made it professional

--- deepmd5.py
+++ deepmd5.py
@@ -3,30 +3,44 @@ import glob
 import sys
 import os
 
-def md5(fname):
+def calculate_md5_checksum(file_path):
+    """
+    Calculate the MD5 checksum of a file.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        str: The MD5 checksum as a hexadecimal string.
+    """
     hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
             hash_md5.update(chunk)
-            
-    print(hash_md5.hexdigest())
+
     return hash_md5.hexdigest()
-    
 
-pathOut = sys.argv[2]
-if os.path.exists(pathOut):
-  os.remove(pathOut)
+def main():
+    # Check if the output file already exists and remove it if it does
+    output_path = sys.argv[2]
+    if os.path.exists(output_path):
+        os.remove(output_path)
 
-pathIn = sys.argv[1]
-if pathIn[len(pathIn)-1] == '/':
-   path1 = pathIn + "*"
-else:
-   path1 = pathIn + "/*"
+    # Get the input path and expand it to include all files in the directory
+    input_path = sys.argv[1]
+    if input_path.endswith('/'):
+        search_pattern = input_path + "*"
+    else:
+        search_pattern = input_path + "/*"
 
-fileList = glob.glob(path1)
-print(fileList)
+    file_list = glob.glob(search_pattern)
+    print("List of files:", file_list)
 
-f = open(pathOut, "x")
+    # Open the output file for writing
+    with open(output_path, "x") as output_file:
+        for file_path in file_list:
+            md5_checksum = calculate_md5_checksum(file_path)
+            output_file.write(md5_checksum + "\n")
 
-for x in fileList:
-   f.write(md5(x)+"\n")
+if __name__ == "__main__":
+    main()
